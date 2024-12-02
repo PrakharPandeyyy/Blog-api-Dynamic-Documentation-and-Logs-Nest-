5b107419864f73cbfcadf19d9b5105e8f6bb8fd3
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-12-03 01:52:27 +0530

    added config management

--- dist/users/providers/users.service.d.ts
+++ dist/users/providers/users.service.d.ts
@@ -3,10 +3,12 @@ import { AuthService } from 'src/auth/providers/auth.service';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { createUserDto } from '../dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
 export declare class UsersService {
     private readonly userRepository;
     private readonly authService;
-    constructor(userRepository: Repository<User>, authService: AuthService);
+    private readonly configService;
+    constructor(userRepository: Repository<User>, authService: AuthService, configService: ConfigService);
     createUser(createUserDto: createUserDto): Promise<User>;
     findAll(getUserParamDto: GetUserParamDto, limit: number, page: number): {
         firstName: string;
